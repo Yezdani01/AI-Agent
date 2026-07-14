d55c38d5cd70629f74a6169deda44537dc8ca9e5
Yezdani01 <yezdanihamza@gmail.com>
2026-07-14 18:28:50 +0530

    AI Agents Response - I need some response to understand and act on it , without response I cant understand and I cant figure out

--- agents/agent1.py
+++ agents/agent1.py
@@ -6,4 +6,5 @@ from dotenv import load_dotenv
 from agentspan.agents import Agent, AgentRuntime, ConversationMemory, run, tool
 
 load_dotenv()
-
+logging.basicConfig(level=logging.WARNING)
+#so that we dont get flooded my unnecessary logs on our terminal and just a necessary response is all we need..
