41498c4c82050144029d3175e18bbf4f9b1925fd
Yezdani01 <yezdanihamza@gmail.com>
2026-07-14 18:36:45 +0530

    Trying to get relevant response -sometimes all  we need is some response to understand and act on it , without response we cant understand and  cant figure out and end up disappointed

--- agents/agent1.py
+++ agents/agent1.py
@@ -8,3 +8,6 @@ from agentspan.agents import Agent, AgentRuntime, ConversationMemory, run, tool
 load_dotenv()
 logging.basicConfig(level=logging.WARNING)
 #so that we dont get flooded my unnecessary logs on our terminal and just a necessary response is all we need..
+logging.getLogger("agentspan").setLevel(logging.WARNING)
+logging.getLogger("conductor").setLevel(logging.WARNING)
+#setting the logging level to WARNING so that we dont get bunch of info logs which are not relevant to us, we need relevant information to understand and act on in order to move forward while understanding the project, debugging, understanding its working etc
